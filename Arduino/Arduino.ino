83580696d82072ed01fcf7486ab43d5f7f660cf7
Sergey Mezentsev <mr.sm2002@mail.ru>
2023-06-20 23:40:13 +0500

    Add the communication between esp8266 and Nano-station to send/receive number of modules from the user

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,14 +1,19 @@
+#include "GBUSmini.h"
+
 // define pins
-#define TX_PIN 10               // pin connected to tranciever module
-#define RX_PIN  5               // pin connected to receiver module
-#define LED_PRESENCE_PIN 12     //This led allows to know if all modules are present
+#define TX_PIN 10                     // pin connected to tranciever module
+#define RX_PIN  5                     // pin connected to receiver module
+//#define LED_PRESENCE_PIN A0           //This led allows to know if all modules are present      //Uncomment if u need dynamic indication of modules presence
+#define GET_NUMBER_OF_MODULES_PIN 4   //This pin allows to get number of modules
+#define PRESENCE_STATE_PIN  7         //This allows to show presence to the esp8266
+
+byte numberOfModules = 0;             //Maximum number of modules that will be checked
 
 //settings for the radioprotocol
 #define RF_ERR 500              //The shortest duration of the pulse
 #define NUM_OF_START_PULSES 3   //Number of the shortest durations (RF_ERR) in every pulse in the teacher's message
 #define RF_START_TEACHER  5     // Number of start pulses in the teacher's message
 #define RF_START_CHILD  3       //Number of start pulses in preambule of every children's message
-#define MAX_NUM_OF_MODULES 1    //Maximum number of modules that will be checked
 
 //-----------------------------------------------------------------------------------------------------
 //function for teacher's message
@@ -23,18 +28,6 @@ void teacherMSGSend(void)
   digitalWrite(LED_BUILTIN, LOW);
   delayMicroseconds(NUM_OF_START_PULSES*RF_ERR);
   }
-}
-//-----------------------------------------------------------------------------------------------------
-//getting coords from GPS-tracker
-void GetGPSCoords(void)
-{
-  return;
-}
-//-----------------------------------------------------------------------------------------------------
-//sending coords to server
-void sendGPSCoords(void)
-{
-  return;
 }
 
 //-----------------------------------------------------------------------------------------------------
@@ -56,12 +49,12 @@ uint8_t getAddr(void)
 
   uint8_t data = 1;
   if (flag){
-    delayMicroseconds(3 * RF_ERR - 100);
+    delayMicroseconds(3 * RF_ERR - 200);
     // read ADDR
     for(uint8_t i=8; i; i--) {
-      duration = pulseIn(RX_PIN, HIGH, 5 * RF_ERR);
+      duration = pulseIn(RX_PIN, HIGH, 14 * RF_ERR);
       data <<= 1;
-      if (duration > (2*RF_ERR - 100)) data |= 1;
+      if (duration > (2*RF_ERR - 200)) data |= 1;
     }
   }
 
@@ -74,18 +67,18 @@ uint8_t getAddr(void)
 //-----------------------------------------------------------------------------------------------------
 //Checking presence of all modules
 uint8_t arrPresence[32] = {0};    //If the counter here is more than 5, the module hasn't been available more than for 5 cycles
-bool absentModules[32] = {0};     // If the counter is more than 5, so here we will se the flag that shows us absence of the module
+bool absentModules[32] = {0};     // If the counter is more than 5, so here we will set the flag that shows us absence of the module
 
 void incArr()
 {
-  for (uint8_t i = 0; i < MAX_NUM_OF_MODULES; i++)
+  for (uint8_t i = 0; i < numberOfModules; i++)
     if(arrPresence[i] <= 5) arrPresence[i]++;
 }
 
 bool allModulesPresent()
 {
   bool presenceFlag = 1;
-  for (uint8_t i = 0; i < MAX_NUM_OF_MODULES; i++)
+  for (uint8_t i = 0; i < numberOfModules; i++)
     if (arrPresence[i] >= 5)
     {
       presenceFlag = 0;
@@ -102,7 +95,7 @@ void printAbsentModules()
 {
   Serial.println("---ALERT!---");
   Serial.println("Absent Modules:");
-  for (uint8_t i = 0; i < MAX_NUM_OF_MODULES; i++)
+  for (uint8_t i = 0; i < numberOfModules; i++)
     if(absentModules[i] == true)
     {
       Serial.print("Module:\t"); Serial.println(i+1); Serial.print("\n");
@@ -111,6 +104,9 @@ void printAbsentModules()
 
 //-----------------------------------------------------------------------------------------------------
 void setup() {
+
+  Serial.begin(9600);
+  
   //Configuring pins
   pinMode(TX_PIN, OUTPUT);
   digitalWrite(TX_PIN, LOW);
@@ -120,12 +116,20 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
-  pinMode(LED_PRESENCE_PIN, OUTPUT);
-  digitalWrite(LED_PRESENCE_PIN, LOW);
+  //pinMode(LED_PRESENCE_PIN, OUTPUT);    //Uncomment if u need dynamic indicator of modules presence
+  //digitalWrite(LED_PRESENCE_PIN, LOW);  //Uncomment if u need dynamic indicator of modules presence
 
-  Serial.begin(9600);
+  pinMode(PRESENCE_STATE_PIN, OUTPUT);
+  digitalWrite(PRESENCE_STATE_PIN, LOW);
+
+  pinMode(GET_NUMBER_OF_MODULES_PIN, INPUT_PULLUP);   //This is required for "GBUS" library
+
+  while( !(GBUS_read(GET_NUMBER_OF_MODULES_PIN, 3, &numberOfModules, sizeof(numberOfModules))) )  ;
+  
+  Serial.print("got: "); Serial.println(numberOfModules);
 
 }
+
 void loop(){
   
   teacherMSGSend();
@@ -159,19 +163,19 @@ void loop(){
   //Check for presence of all modules
   if(!allModulesPresent())
   {
+    digitalWrite(PRESENCE_STATE_PIN, HIGH);   //Allows to send the absence signal to esp8266
+    
     printAbsentModules();
+
+    //Uncomment if u need dynamic indicator of modules presence
+    /*
     digitalWrite(LED_PRESENCE_PIN, HIGH);
     delay(500);
     digitalWrite(LED_PRESENCE_PIN, LOW);
+    */
   }
-
-  //Work with GPS coords
-  GetGPSCoords();
-  sendGPSCoords();
+  else { digitalWrite(PRESENCE_STATE_PIN, LOW); }
+  
   timeLeft = 1600;
   delay(200);
-
-
-  
-  
 }
